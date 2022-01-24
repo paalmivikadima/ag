a90a347532a432e1acdcf0bb51106a4ef70aa748
aleksandr <paalmivika@gmail.com>
2022-01-24 20:46:42 +0300

    u

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%v\n", time)
+	os.Exit(0)
