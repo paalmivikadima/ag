1bed03a47e99a7f7f10edc4a85c4cab4f0dca0f3
aleksandr <paalmivika@gmail.com>
2022-02-13 20:13:12 +0300

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/paalmivikadima/ag
 
 go 1.17
+
+require github.com/beevik/ntp v0.3.0
+
+require (
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"log"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	
+	l := log.New(os.Stderr, "dz1-", log.Ldate|log.Ltime)
+	if time, err := ntp.Time("0.beevik-ntp.pool.ntp.org"); err != nil {
+		l.Fatalln(err)
+	} else {
+		fmt.Println(time.Format("Mon Jan 2 15:04:05 -0700 MST 2006"))
 	}
+}
